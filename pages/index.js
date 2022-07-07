01d11a4b666f2ae09a233502f4ba1d7cfc6b8d92
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-07 11:59:10 -0700

    add styles using modules

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
         accusantium reiciendis? Suscipit facilis recusandae possimus
         reprehenderit nihil a vitae animi voluptatem aliquid, cum, velit
         repellendus.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
         accusantium reiciendis? Suscipit facilis recusandae possimus
         reprehenderit nihil a vitae animi voluptatem aliquid, cum, velit
         repellendus.
       </p>
       <Link href="/devs">
-        <a>See Dev Listing</a>
+        <a className={styles.btn}>See Dev Listings</a>
       </Link>
     </div>
   );
