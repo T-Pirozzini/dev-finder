1cdf9658832d86883d6c732cffae037122041f46
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-07 11:08:23 -0700

    setup next app

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, accusantium reiciendis? Suscipit facilis recusandae possimus reprehenderit nihil a vitae animi voluptatem aliquid, cum, velit repellendus.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, accusantium reiciendis? Suscipit facilis recusandae possimus reprehenderit nihil a vitae animi voluptatem aliquid, cum, velit repellendus.</p>
+    </div>
+  );
+}
+
+export default About;
