ea81dae9599c1387536b0d05b706439a30bbf87b
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-08 13:21:09 -0700

    display dev page info on dev listing name click

--- pages/devs/[id].jsx
+++ pages/devs/[id].jsx
@@ -1,13 +1,36 @@
 export const getStaticPaths = async () => {
-  const res = await fetch('http://jsonplaceholder.typicode.com/users')
-  const data = r
-  es.json()
+  const res = await fetch('http://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map(dev => {
+    return {
+      params: { id: dev.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('http://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { dev: data }
+  }
+}
+
+const Details = ({ dev }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ dev.name }</h1>
+      <p>{ dev.email }</p>
+      <p>{ dev.website }</p>
+      <p>{ dev.address.city }</p>
     </div>
   );
 }
